84541cc43bae04f3035c0a5d1ca48829826fe973
xwen <xwenx66@gmail.com>
2025-06-13 21:24:31 -0700

    update file

---
+++ MeadowDream/MeadowDreamApp.swift
@@ -0,0 +1,15 @@
+//
+//  MeadowDreamApp.swift
+//  MeadowDream
+
+
+import SwiftUI
+
+@main
+struct MeadowDreamApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
